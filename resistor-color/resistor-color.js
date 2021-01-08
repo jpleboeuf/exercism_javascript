8b4b4cdca04c7fc4cf05dc110613fc36aa447839
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-01-08 23:34:27 +0100

    Solve exercise "Resistor Color"

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,10 +1,18 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const CV = {
+    'black':   0,
+    'brown':   1,
+    'red':     2,
+    'orange':  3,
+    'yellow':  4,
+    'green':   5,
+    'blue':    6,
+    'violet' : 7,
+    'grey':    8,
+    'white':   9,
+  };
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const colorCode = (color) => {
+  return CV[color];
 };
 
-export const COLORS = undefined;
+export const COLORS = Object.keys(CV);
